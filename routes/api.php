30f6a79811921d4809e8a2082de1f5224b868bd8
amine-cheraitia <39153775+amine-cheraitia@users.noreply.github.com>
2023-04-27 22:44:10 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\ProductTestController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
